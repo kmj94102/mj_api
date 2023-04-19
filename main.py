b8ce0565b223bb7b81bdcf6a8e146c4cce490da0
김민재 <kmj94102@naver.com>
2023-04-19 19:41:59 +0900

    테스트 페이지

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+@app.get("/test")
+def test():
+    return "테스트 페이지"
