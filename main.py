25d806ad9198445e0dabee1dbeec109fb21331f8
김민재 <kmj94102@naver.com>
2023-05-11 10:04:26 +0900

    포켓몬 검색 추가

--- main.py
+++ main.py
@@ -47,13 +47,13 @@ async def insert_pokemon(item: Pokemon):
 
 # 포켓몬 리스트 조회
 @app.get("/pokemonList")
-async def read_pokemon_list(skip: int = 0, limit: int = 100):
+async def read_pokemon_list(name: str = "", skip: int = 0, limit: int = 100):
     session.commit()
 
     list = session.query(PokemonTable.index, PokemonTable.number, PokemonTable.name, PokemonTable.spotlight,
                          PokemonTable.shinySpotlight) \
-        .offset(skip).limit(limit).all()
-    total_size = session.query(PokemonTable).count()
+        .filter(PokemonTable.name.like(f"%{name}%")).offset(skip).limit(limit).all()
+    total_size = session.query(PokemonTable).filter(PokemonTable.name.like(f"%{name}%")).count()
     return {
         "list": list,
         "totalSize": total_size
