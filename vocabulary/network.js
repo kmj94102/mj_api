70e1005a59bd5a4ad8a17968c1adff0ad02649b1
김민재 <kmj94102@naver.com>
2023-09-18 19:10:18 +0900

    - 단어장 데이터 등록 web 페이지 서버 통신 추가

--- vocabulary/network.js
+++ vocabulary/network.js
@@ -8,42 +8,41 @@ document.addEventListener("DOMContentLoaded", function() {
     const groupMeaning = document.getElementById("groupMeaning");
 
     submit.addEventListener("click", function(event){
-        // 토스트 메시지 표시
-        toast.textContent = day.value + "ㅎㅎ";
-        toast.style.visibility = "visible";
-
-        // 토스트 메시지 2초 후에 숨기기
-        setTimeout(function() {
-            toast.style.visibility = "hidden";
-        }, 2000);
-
-        fetch('https://port-0-mj-api-e9btb72blgnd5rgr.sel3.cloudtype.app', {
+        const data = {
+            day: day.value,
+            name: group.value,
+            meaning: groupMeaning.value,
+            modify: modify.value
+        }
+        fetch('https://port-0-mj-api-e9btb72blgnd5rgr.sel3.cloudtype.app/vocabulary/group/insert', {
                  method: 'POST',
                  headers: {
                      'Content-Type': 'application/json'
                  },
-                 body: JSON.stringify({
-                     day,
-                     group,
-                     word,
-                     meaning,
-                     hint,
-                     additional
-                 })
+                 body: JSON.stringify(data)
              })
              .then(response => response.json())
              .then(data => {
-                 // Handle the response from the server
-                 console.log(data);
-                 // Clear input fields
-                 document.getElementById("day").value = '';
-                 document.getElementById("word").value = '';
-                 document.getElementById("meaning").value = '';
-                 document.getElementById("hint").value = '';
-                 document.getElementById("additional").value = '';
+                console.log(data);
+                // 토스트 메시지 표시
+                toast.textContent = data;
+                toast.style.visibility = "visible";
+
+                // 토스트 메시지 2초 후에 숨기기
+                setTimeout(function() {
+                    toast.style.visibility = "hidden";
+                }, 2000);
              })
              .catch(error => {
-                 console.error('Error:', error);
+                console.error('Error:', error);
+                // 토스트 메시지 표시
+                toast.textContent = "등록 실패";
+                toast.style.visibility = "visible";
+
+                // 토스트 메시지 2초 후에 숨기기
+                setTimeout(function() {
+                    toast.style.visibility = "hidden";
+                }, 2000);
              });
     });
 });
@@ -60,14 +59,45 @@ document.addEventListener("DOMContentLoaded", function() {
     const additional = document.getElementById("additional");
 
     submit.addEventListener("click", function(event){
-        // 토스트 메시지 표시
-        toast.textContent = day.value;
-        toast.style.visibility = "visible";
+        const data = {
+            day: day.value,
+            group: group.value,
+            word: word.value,
+            meaning: wordMeaning.value,
+            hint: hint.value,
+            additional: additional.value
+        }
 
-        // 토스트 메시지 2초 후에 숨기기
-        setTimeout(function() {
-            toast.style.visibility = "hidden";
-        }, 2000);
+        fetch('https://port-0-mj-api-e9btb72blgnd5rgr.sel3.cloudtype.app/vocabulary/insert', {
+                 method: 'POST',
+                 headers: {
+                     'Content-Type': 'application/json'
+                 },
+                 body: JSON.stringify(data)
+             })
+             .then(response => response.json())
+             .then(data => {
+                console.log(data);
+                // 토스트 메시지 표시
+                toast.textContent = data;
+                toast.style.visibility = "visible";
+
+                // 토스트 메시지 2초 후에 숨기기
+                setTimeout(function() {
+                    toast.style.visibility = "hidden";
+                }, 2000);
+             })
+             .catch(error => {
+                console.error('Error:', error);
+                // 토스트 메시지 표시
+                toast.textContent = "등록 실패";
+                toast.style.visibility = "visible";
+
+                // 토스트 메시지 2초 후에 숨기기
+                setTimeout(function() {
+                    toast.style.visibility = "hidden";
+                }, 2000);
+             });
 
         word.value = ""
         wordMeaning.value = ""
