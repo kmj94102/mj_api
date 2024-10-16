2064f5fcb34dfafe3e1d6e1546566ad8a235894a
김민재 <kmj94102@naver.com>
2024-10-16 22:10:16 +0900

    - token api post -> get으로 변경 및 로그

--- api/adProfit.py
+++ api/adProfit.py
@@ -7,6 +7,8 @@ router = APIRouter()
 
 @router.get("/insert/tokenInfo", summary="게시글 등록")
 async def insert_board(email: str, refreshToken: str, accessToken: str, device: str) -> str:
+    print(f"\n\n\n+++++ email: {email}\n refreshToken: {refreshToken}\n"
+          f"accessToken: {accessToken}\n device: {device}\n \n\n\n")
     adProfit = AdProfit(email=email, refreshToken=refreshToken, accessToken=accessToken, device=device).toTable()
     session.add(adProfit)
     session.commit()
