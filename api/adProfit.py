088d889579beec80bd3aca8c8a78da6fe801379e
김민재 <kmj94102@naver.com>
2024-10-16 21:01:26 +0900

    - token test 로그

--- api/adProfit.py
+++ api/adProfit.py
@@ -7,6 +7,7 @@ router = APIRouter()
 
 @router.post("/insert/tokenInfo", summary="게시글 등록")
 async def insert_board(item: AdProfit) -> str:
+    print(f"\n\n\n+++++ {item}\n\n\n")
     adProfit = item.toTable()
     session.add(adProfit)
     session.commit()
