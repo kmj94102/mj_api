6ea79e8512ff35d9b2874a56a1990e57fd1b1b6f
김민재 <kmj94102@naver.com>
2025-10-14 15:35:03 +0900

    - 디지몬 추가 웹 사이트 api 적용

--- web/digimon/network.js
+++ web/digimon/network.js
@@ -12,7 +12,7 @@ addBtn.addEventListener('click', () => {
   newGroup.innerHTML = `
     <img class="deleteBtn" src="close.svg" width="24px" height="24px">
     <div>이름</div>
-    <input type="text" id="name">
+    <input type="text" name="name">
     <div>오픈 여부</div>
     <input type="radio" name="isOpen-${index}" id="isOpenTrue-${index}">
     <label for="isOpenTrue-${index}">True</label>
@@ -33,24 +33,61 @@ container.addEventListener('click', (e) => {
 });
 
 saveBtn.addEventListener('click', () => {
+  const groupName = document.getElementById('group').value;
+  const level = parseInt(document.getElementById('level').value);
+  const isTranscend = document.getElementById('isTranscendTrue').checked;
   const groups = document.querySelectorAll('.digimonInfo');
-  const data = [];
+  const list = [];
 
   groups.forEach((group, i) => {
-    const example = group.querySelector('input[name="example"]').value.trim();
-    const meaning = group.querySelector('input[name="meaning"]').value.trim();
-    const hint = group.querySelector('input[name="hint"]').value.trim();
-    const selected = group.querySelector('input[type="radio"]').checked;
+    const name = group.querySelector('input[name="name"]').value.trim();
+    const isOpen = group.querySelector('input[type="radio"]').checked;
 
-    data.push({
-      id: i + 1,
-      example,
-      meaning,
-      hint,
-      selected
-    });
+    if(name && isOpen) {
+      list.push({
+        name: name,
+        isOpen: isOpen,
+        isTranscend: isTranscend,
+        level: level
+      });
+    }
+
   });
+  const data = {
+    groupName: groupName,
+    list: list
+  };
+
+  if (!groupName) {
+    alert("그룹 이름을 입력해주세요.");
+    return;
+  }
+
+  if (isNaN(level)) {
+    alert("레벨 값이 올바르지 않습니다.");
+    return;
+  }
+
+  if (list.length === 0) {
+    alert("등록할 디지몬 정보가 없습니다.");
+    return;
+  }
 
   console.log('입력된 데이터:', data);
-  alert(`총 ${data.length}개의 항목 중 선택된 항목: ${data.find(d => d.selected)?.id || '없음'}`);
+
+  fetch('http://127.0.0.1:8000/digimon/insert/dmo', {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(data)
+   })
+   .then(response => response.json())
+   .then(_ => {
+      alert(groupName + ' 등록 성공');
+   })
+   .catch(error => {
+      alert('등록 실패');
+      console.error('Error:', error);
+   });
 });
